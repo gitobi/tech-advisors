a6702cc20d9da9f345fb2e09f905bc3b8289f8aa
Rui Onodera <deraru@gmail.com>
2015-05-15 11:36:38 +0900

    Add BrowserSync

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,17 @@
 /// <reference path="./typings/tsd.d.ts"/>
-import gulp = require('gulp');
 
-gulp.task('default', () => {
-    console.log('Hello world.');
+import gulp = require('gulp');
+import browserSync = require('browser-sync');
+
+var reload = browserSync.reload;
+
+gulp.task('html-watch', reload);
+gulp.task('serve', () => {
+    browserSync({
+        server: 'app'
+    });
+    gulp.watch('app/**/*.html', [ 'html-watch' ])
 });
+
+gulp.task('default', ['serve']);
+
